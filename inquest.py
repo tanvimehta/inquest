941abd8d290c370fc338924d4ea7df5c87b04675
Siddharth Zaveri <6thfebsid@gmail.com>
2014-10-10 13:33:12 -0400

    History table works now
    Added updateWordList function to add new tuples in history or update
    existing tuples
    Added createHistoryTable to create html string for two 20 history
    entries

--- inquest.py
+++ inquest.py
@@ -28,31 +28,32 @@ def do_inquest() :
     query = request.forms.get('keywords')
     words = query.split()
     words.sort()
-    resultsTable = "<h3>" + query + "</h3><table><tr><td>Word</td><td>Count</td></tr>"
+    resultsTable = "<h3>" + query + "</h3><table style = \"float: left\" id = \"result\"><tr><td>Word</td><td>Count</td></tr>"
     leftIndex = 0
     count = 0
     if len(words) == 1:
         resultsTable = resultsTable + "<tr><td>" + words[0] + "</td>" + "<td>1</td></tr>"
         updateWordList(words[0],1)
-
-    for index in range(0, len(words)-1):
-        if words[index] != words[index+1]:
-            count = index - leftIndex + 1
-            leftIndex = index + 1
-            resultsTable = resultsTable + "<tr><td>" + words[index] + "</td>" + "<td>" + str(count) + "</td></tr>"
-            updateWordList(words[index],count)
-
-    
-    if (words[len(words)-1] != words[len(words)-2]):
-        count = 1
     else:
-        count = len(words) - leftIndex
-    
-    updateWordList(words[len(words)-1],)
-    resultsTable = resultsTable + "<tr><td>" + words[len(words)-1] + "</td>" + "<td>" + str(count) + "</td></tr>"
-    wordList = wordList + [words[len(words) - 1], len(words) - leftIndex]
-    resultsTable = resultsTable + "</table>"
-    return "<body>" + resultsTable + "</body>"
+        for index in range(0, len(words)-1):
+            if words[index] != words[index+1]:
+                count = index - leftIndex + 1
+                leftIndex = index + 1
+                resultsTable = resultsTable + "<tr><td>" + words[index] + "</td>" + "<td>" + str(count) + "</td></tr>"
+                updateWordList(words[index],count)
+
+        
+        if (words[len(words)-1] != words[len(words)-2]):
+            count = 1
+        else:
+            count = len(words) - leftIndex
+        
+        updateWordList(words[len(words)-1],count)
+        resultsTable = resultsTable + "<tr><td>" + words[len(words)-1] + "</td>" + "<td>" + str(count) + "</td></tr>"
+        resultsTable = resultsTable + "</table>"
+    wordList.sort(key=lambda x: x[1],reverse=True)
+    historyTable = createHistoryTable()
+    return "<body>" + resultsTable + historyTable + "</body>"
 
 def getKey(item):
     return item[0]
@@ -61,8 +62,27 @@ def getValue(item):
     return item[1]
 
 def updateWordList (word, count):
-    if any (word in tuple for tuple in wordList):
-        
+    global wordList
+    tmpList = []
+    updated = False 
+    for (w, c) in wordList: 
+        if w == word:
+            tmpList.append((w, c + count))
+            updated = True
+        else: 
+            tmpList.append((w, c))
+
+    if updated == False:
+        tmpList.append((word,count))
+    wordList[:] = tmpList
+
+def createHistoryTable ():
+    global wordList
+    historyTable = "<table style = \"float: right\" id = \"history\"><tr><td>Word</td><td>Count</td></tr>"
+    max_output = min(20,len(wordList))
+    for i in range(0, max_output):
+        historyTable = historyTable + "<tr><td>" + wordList[i][0] + "</td>" + "<td>" + str(wordList[i][1]) + "</td></tr>"
+    return historyTable
 
 # l = [[2, 3], [6, 7], [3, 34], [24, 64], [1, 43]]
 # sorted(l, key=getKey)
