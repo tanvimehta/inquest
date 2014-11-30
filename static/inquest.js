bc5eb11b32082a8abe7caf34cecd81fa3c215dae
Tanvi Mehta <tanvimehta@Tanvis-MacBook-Pro.local>
2014-11-30 15:39:03 -0500

    Initial type ahead code.

--- static/inquest.js
+++ static/inquest.js
@@ -26,11 +26,23 @@ function autoComp() {
 
 function showOptions(wordList) {
   document.getElementById("dropdown").style.visibility = "visible";
+  document.getElementById("dropdown").innerHTML = "";
   for (i = 0; i < numSuggestions; i++) {
-    if (wordList[i] != undefined)
+    if (wordList[i] != undefined) {
+      document.getElementById("dropdown").size = i + 1;
       document.getElementById("dropdown").innerHTML = document.getElementById("dropdown").innerHTML + "<option value=\"word" + i + "\">" + wordList[i] + "</option>";
+    }
     else
       break;
+  }
+  
+  if (wordList[0] != undefined) {
+    currentValue = document.getElementById("keywords").value;
+    newValue = wordList[0];
+    start = currentValue.length;
+    end = newValue.length - 1;
+    document.getElementById("keywords").value = newValue;
+    createSelection(document.getElementById("keywords"), start, end);
   }
 }
 
@@ -38,3 +50,21 @@ function applySelect() {
   var drop = document.getElementById("dropdown");
   document.getElementById("keywords").value = drop.options[drop.selectedIndex].text;
 }
+
+function createSelection(field, start, end) {
+    if( field.createTextRange ) {
+      var selRange = field.createTextRange();
+      selRange.collapse(true);
+      selRange.moveStart('character', start);
+      selRange.moveEnd('character', end);
+      selRange.select();
+      field.focus();
+    } else if( field.setSelectionRange ) {
+      field.focus();
+      field.setSelectionRange(start, end);
+    } else if( typeof field.selectionStart != 'undefined' ) {
+      field.selectionStart = start;
+      field.selectionEnd = end;
+      field.focus();
+    }
+  }
